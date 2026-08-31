877b9bea1207b7b72e32d969cf09379dbe7d6f5b
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2026-08-31 23:42:25 +0530

    fix: load demo polish overrides

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./styles.css";
 import "./domain.css";
 import "./trust.css";
+import "./demo-polish.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
