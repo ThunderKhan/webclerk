6e48728af2a9e78bc9fe8a01127d157b97be9cde
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2026-09-01 23:42:57 +0530

    feat(web): route live insurance proof workspace

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import InsuranceProof from "./InsuranceProof";
 import LandingPage from "./LandingPage";
 import "./styles.css";
 import "./domain.css";
 import "./trust.css";
 import "./demo-polish.css";
+import "./insurance-proof.css";
 
 const path = window.location.pathname.replace(/\/+$/, "") || "/";
 const isDemo = path === "/demo";
+const isInsuranceProof = path === "/proof/insurance";
 
 function DemoRoute() {
   return (
@@ -24,6 +27,6 @@ function DemoRoute() {
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {isDemo ? <DemoRoute /> : <LandingPage />}
+    {isDemo ? <DemoRoute /> : isInsuranceProof ? <InsuranceProof /> : <LandingPage />}
   </React.StrictMode>,
 );
