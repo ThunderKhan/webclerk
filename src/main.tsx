df6da9cd050fde70c8fb500028acf3e191cf6474
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2026-09-01 15:32:01 +0530

    feat: route root to landing page and demo to application

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import LandingPage from "./LandingPage";
 import "./styles.css";
 import "./domain.css";
 import "./trust.css";
 import "./demo-polish.css";
 
+const path = window.location.pathname.replace(/\/+$/, "") || "/";
+const isDemo = path === "/demo";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    {isDemo ? <App /> : <LandingPage />}
   </React.StrictMode>,
 );
